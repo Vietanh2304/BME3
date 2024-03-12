a014e7c7a990dd5a176581d6918bc9431e8a1b1c
Vietanh2304 <117104704+Vietanh2304@users.noreply.github.com>
2024-03-12 14:37:51 +0700

    Update display_app.h

--- BME3/lib/display_app/display_app.h
+++ BME3/lib/display_app/display_app.h
@@ -7,6 +7,6 @@ extern TM1637Display display;
 
 void display_clear();
 void display_init();
-void display_num();
+void display_num(int x,uint8_t dot, bool leading_zeros, uint8_t lenght, uint8_t pos );
 
 #endif
